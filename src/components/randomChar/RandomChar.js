21f8fb3cd6e39463bf33a04e121e5eab58e67f89
Shist <shist.pupust@mail.ru>
2023-08-27 18:01:24 +0300

    Now after clicking "try it" state sets to loading = true and error = false

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -28,6 +28,7 @@ class RandomChar extends Component {
   };
 
   updateChar = () => {
+    this.setState({ loading: true, error: false });
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
     this.marvelService
       .getCharacter(id)
