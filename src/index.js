124cd3e5b5326e09c650c37209174408c1f7505b
Shist <shist.pupust@mail.ru>
2023-08-27 13:26:06 +0300

    Added class for working with API, some get requests already work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import './style/style.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+import "./style/style.scss";
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then((resolve) => {
+  console.log(resolve.data.results.forEach((hero) => console.log(hero.name)));
+});
+
+marvelService.getCharacter(1011052).then((resolve) => {
+  console.log(resolve.data.results.forEach((hero) => console.log(hero.name)));
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
