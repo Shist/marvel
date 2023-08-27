3d73f4abed2b6045c1240a85579fedb76fb12451
Shist <shist.pupust@mail.ru>
2023-08-27 14:43:29 +0300

    Now API puts random char to the page with name, descr and img

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then((resolve) => {
-  console.log(resolve.data.results.forEach((hero) => console.log(hero.name)));
-});
-
-marvelService.getCharacter(1011052).then((resolve) => {
-  console.log(resolve.data.results.forEach((hero) => console.log(hero.name)));
-});
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,14 +12,28 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const result = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return result.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacter = async (id) => {
+    const result = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(result.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].wiki,
+    };
   };
 }
 
