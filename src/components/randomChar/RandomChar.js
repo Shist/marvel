c913271a895b5c32c8ef8b5b815f3a1f4f7bc43f
Shist <shist.pupust@mail.ru>
2023-08-27 17:13:42 +0300

    Added feature with updating character by clicking button "try it"

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -29,7 +29,6 @@ class RandomChar extends Component {
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    console.log(id);
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
@@ -54,7 +53,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button className="button button__main" onClick={this.updateChar}>
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
